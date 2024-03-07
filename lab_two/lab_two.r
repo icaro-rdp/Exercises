5df637401410e19b2874dc0bab16a159abdcf2ff
Icaro Re Depaolini <igher97@gmail.com>
2024-03-07 13:55:15 +0100

    setting up for lab_2

---
+++ lab_two/lab_two.r
@@ -0,0 +1,4 @@
+setwd("/Users/icaroredepaolini/Library/CloudStorage/GoogleDrive-icaro.redepaolini@studenti.unitn.it/Il mio Drive/First year/Secondo semestre/Quantitative/Exercises/lab_two/")
+
+
+phones_df <- read.csv("phones.csv")
