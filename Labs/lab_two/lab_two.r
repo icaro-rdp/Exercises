9b82eaaac6c05273bebe9e0eb169726f10001b43
Icaro Re Depaolini <igher97@gmail.com>
2024-03-19 14:20:33 +0100

    updated console print

--- Labs/lab_two/lab_two.r
+++ Labs/lab_two/lab_two.r
@@ -10,19 +10,16 @@ press_displays <- factor(phones_df$PressDisplay)
 
 # Exercise 1.3 Assign to the variables RAM and Attractiveness an ordinal representation using the appropriate R function.
 
-ram <- factor(phones_df$RAM, ordered = TRUE, levels = c(2, 4, 6))
-attractiveness <- factor(phones_df$Attractiveness, ordered = TRUE, levels = c("Poor", "Modest", "Sufficient", "Appreciable", "High"))
+ram <- ordered(phones_df$RAM, levels = c(2, 4, 6))
+attractiveness <- ordered(phones_df$Attractiveness, levels = c("Poor", "Modest", "Sufficient", "Appreciable", "High"))
 
-# Or if u want u can use the ordered function (wrapper for factor)
-
-ram <- ordered(phones_df$RAM)
-attractiveness <- ordered(phones_df$Attractiveness)
-
+print("Exercise 1.2 and 1.3 : categorical and ordinal representations")
 print(builds)
 print(displays)
 print(press_displays)
 print(ram)
 print(attractiveness)
+cat("\n")
 
 
 # Exercise 1.4 Construct and show the values of the contingency table obtained on the basis of the categorical variables Build (represented in the rows of the table) and Display (represented in the columns of the table). By using the same contingency table derive a) the corresponding proportion table b) the observed conditional probability table P(Build|Display)
@@ -35,15 +32,19 @@ prop_table_build_display <- table(builds, displays) / sum(cont_table_build_displ
 
 prop_table_build_display <- prop.table(cont_table_build_display)
 
+print("Exercise 1.4 : contingency table, proportion table, observed conditional probability table")
 print(cont_table_build_display)
 print(prop_table_build_display)
+cat("\n")
 
 # Exercise 1.5 Summarize the content of the sub-dataframe composed by the quantitative variables Length,
 # Width, and Weight.
 
 summary_length_width_weight <- summary(phones_df[, c("Length", "Width", "Weight")])
 
+print("Exercise 1.5 : summary")
 print(summary_length_width_weight)
+cat("\n")
 
 # Exercise 1.6 Compute the correlation values between a) Length and Width b) Length and Weight c) Width and Weight.
 
@@ -51,14 +52,17 @@ corr_length_width <- cor(phones_df$Length, phones_df$Width)
 corr_length_weight <- cor(phones_df$Length, phones_df$Weight)
 corr_width_weight <- cor(phones_df$Width, phones_df$Weight)
 
+print("Exercise 1.6 : correlation values")
 print(corr_length_width)
 print(corr_length_weight)
 print(corr_width_weight)
-
+cat("\n")
 # Exercise 1.7 Derive the ranges for each quantitative variable (Length, Width, and Weight) by considering only those smartphones that are aluminum-based.
 
 aluminum_based <- phones_df[phones_df$Build == "Alumin", c("Length", "Width", "Weight")]
 
 range_aluminum_based <- apply(aluminum_based, 2, range)
 
+print("Exercise 1.7 : ranges")
 print(range_aluminum_based)
+cat("\n")
